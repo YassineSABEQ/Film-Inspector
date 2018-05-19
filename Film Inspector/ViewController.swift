a6335a7f55332a93bf928737ab16dd01dafc6819
YassineSABEQ <sabeq.yassine15@gmail.com>
2018-05-19 14:17:04 +0000

    Initial Commit

---
+++ Film Inspector/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  Film Inspector
+//
+//  Created by Yassine Sabeq on 5/19/18.
+//  Copyright © 2018 Yassine Sabeq. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
